847ef59e6dcd216fc48ca75a989aee14722502ea
gsc15-alt <gurshaansinghchopra@gmail.com>
2026-04-07 22:11:33 +0530

    Update Q2.js

--- Q2.js
+++ Q2.js
@@ -1,19 +1,26 @@
 let N = parseInt(prompt("Enter N:"));
 let seed = parseInt(prompt("Enter seed:"));
 
-let current = N;
+if (N < 1 || N > 1000000 || seed < 0 || seed > 9) {
+    alert("Invalid Input");
+} 
+else {
 
-for (let i = 0; i < 3; i++) {
-    if (current % 2 === 0) {
-        current = current / 2 + seed;
-    } else {
-        current = current * 3 - seed;
+    let current = N;
+
+    for (let i = 0; i < 3; i++) {
+        if (current % 2 === 0) {
+            current = current / 2 + seed;
+        } else {
+            current = current * 3 - seed;
+        }
     }
-}
-let result = current.toString();
 
-if (current >= 100 && current <= 999 && parseInt(result[1]) === seed) {
-    alert("YES, " + current);
-} else {
-    alert("NO, " + current);
+    let str = current.toString();
+
+    if (current >= 100 && current <= 999 && parseInt(str[1]) === seed) {
+        alert("YES, " + current);
+    } else {
+        alert("NO, " + current);
+    }
 }
